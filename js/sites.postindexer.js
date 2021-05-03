58aeae4492261b6315a161c3fd0c497388c6cb51
Dernerd <dern3rd@outlook.at>
2021-05-03 14:05:20 +0200

    Depracated .click() fix

--- js/sites.postindexer.js
+++ js/sites.postindexer.js
@@ -1,16 +1,18 @@
 function pi_loadsiteeditform() {
-	tb_show(postindexer.siteedittitle, jQuery(this).attr('href'));
-	return false;
+    tb_show(postindexer.siteedittitle, jQuery(this).attr('href'));
+    return false;
 }
 
 function pi_loadsitesummary() {
-	tb_show(postindexer.sitesummarytitle, jQuery(this).attr('href'));
-	return false;
+    tb_show(postindexer.sitesummarytitle, jQuery(this).attr('href'));
+    return false;
 }
-
+/*Depracated .click() fix*/
 function pi_sitesready() {
-	jQuery('a.postindexersiteeditlink').click(pi_loadsiteeditform);
-	jQuery('a.postindexersitesummarylink').click(pi_loadsitesummary);
+    //jQuery('a.postindexersiteeditlink').click(pi_loadsiteeditform);
+    jQuery('a.postindexersiteeditlink').on('click', pi_loadsiteeditform);
+    //jQuery('a.postindexersitesummarylink').click(pi_loadsitesummary);
+    jQuery('a.postindexersitesummarylink').on('click', pi_loadsitesummary);
 }
 
 jQuery(document).ready(pi_sitesready);
